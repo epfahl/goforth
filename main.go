1c2de46b6fc8663f5486aafa7948d3262bfae1c5
eric-pfahl-1m <eric.pfahl@1mplatform.com>
2026-02-24 16:33:34 -0500

    fleshed out docstring and in-line comments

--- main.go
+++ main.go
@@ -1,8 +1,3 @@
-// Notes:
-// - Include proper error propagation so that the REPL displays useful error messages.
-// - When an operation fails in immediate mode, I think the expectation is that prior
-//   successful steps should result in stack changes.
-
 package main
 
 import (
@@ -14,7 +9,7 @@ import (
 )
 
 // `StackOp` is a function that operates on the stack and returns a boolean
-// error.
+// error indicator.
 type StackOp func(*Stack) bool
 
 // `Env` holds the environment of a running program as a map from string keys
@@ -30,9 +25,9 @@ type CompileBuffer struct {
 
 // `State` holds the full execution state of a running program.
 //
-// `env`: program environment
-// `execStack`: execution stack when in immediate mode
-// `CompileBuffer`: struct that captures compile state; `nil` means not compiling
+// `env`: program environment that holds the "words"
+// `stack`: execution stack when in immediate mode
+// `compileBuffer`: struct that captures intermediate compile state; `nil` means not compiling
 type State struct {
 	env           Env
 	stack         Stack
@@ -61,16 +56,33 @@ func tokenize(command string) []string {
 // complete error message.
 func evaluate(tokens []string, state *State) bool {
 	for _, token := range tokens {
+
+		// ":" and ";" start and end word compilation.
+		//
+		// Note that ":" and ";" are treated as special characters, rather than
+		// regular words.
 		switch token {
 		case ":":
+			// Initiazlize the compile buffer.
 			state.compileBuffer = &CompileBuffer{}
 			continue
 		case ";":
+			// When compilation ends, translate the compile buffer into a word in the
+			// environment, and then reset the buffer.
 			compileWord(state.compileBuffer, state.env)
 			state.compileBuffer = nil
 			continue
 		}
 
+		// Compilation is stateful. First, the name is captured, and then
+		// the implementation is collected into a slice that represents the sequence
+		// of operations.
+		//
+		// If an existing word is encountered, it's stack operation is appended to
+		// the buffer.
+		//
+		// If a literal (an integer) is encountered, it is captured in a closure that
+		// pushes the value to the stack when the compiled word is executed.
 		if state.compileBuffer != nil {
 			if state.compileBuffer.name == "" {
 				state.compileBuffer.name = token
@@ -86,6 +98,8 @@ func evaluate(tokens []string, state *State) bool {
 			continue
 		}
 
+		// When not in compilation mode, the token (a word or literal) is interpreted
+		// immediately.
 		if interpretToken(token, state) {
 			return true
 		}
@@ -93,7 +107,8 @@ func evaluate(tokens []string, state *State) bool {
 	return false
 }
 
-// Given a compile buffer, create the compiled word and add it to the buffer.
+// Given a compile buffer, create the compiled word and add it to the
+// evironment.
 func compileWord(buffer *CompileBuffer, env Env) bool {
 	f := func(stack *Stack) bool {
 		// Apply each function in the compile buffer to the stack in sequence.
@@ -111,6 +126,7 @@ func compileWord(buffer *CompileBuffer, env Env) bool {
 	return false
 }
 
+// Return a closure that pushes a literal (integer) onto the stack.
 func makePushInt(value int64) StackOp {
 	return func(stack *Stack) bool {
 		stack.Push(value)
@@ -118,7 +134,7 @@ func makePushInt(value int64) StackOp {
 	}
 }
 
-// Evaluate a single token against the current stack.
+// Evaluate a single token against the current state.
 //
 // Note: This returns an error boolean, which can later be elevated to a
 // complete error message.
@@ -139,6 +155,7 @@ func interpretToken(token string, state *State) bool {
 	return true
 }
 
+// Start the REPL.
 func repl(prompt string, state *State) {
 	command := read(prompt)
 	if err := evaluate(command, state); err {
